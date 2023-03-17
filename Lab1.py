eeacabab00fbfd3a6f043d30b31541db011ae7b8
Karina <simon-karina@bk.ru>
2023-03-17 10:51:06 +0600

    done Lab1

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+from bs4 import BeautifulSoup # импортируем библиотеку BeautifulS
+import requests # импортируем библиотеку requests
+import fake_useragent
+def parse():
+    ua= fake_useragent.UserAgent()
+    headers={"User-Agent": ua.random}
+    url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250' # передаем необходимы URL адрес
+    page = requests.get(url, headers=headers)
+    print(page.status_code)
+    soup = BeautifulSoup(page.text, "html.parser")
+    map = {}
+    block = soup.findAll('tr')
+    for data in block:
+        movie=data.find('td', class_='titleColumn')
+        rating=data.find('td', class_='ratingColumn imdbRating')
+        if movie is not None or rating is not None:
+            mov = " ".join(movie.text.replace('\n', '').split())
+            rat = rating.text.replace('\n', '')
+            map[mov] = rat
+
+    print(map)
---
+++ main.py
@@ -0,0 +1,4 @@
+import Lab1
+
+if __name__ == '__main__':
+    Lab1.parse()
